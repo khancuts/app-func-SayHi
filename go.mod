7eefbea91ab85cb1e420a60bd13af63ecf784296
khancuts <80384823+khancuts@users.noreply.github.com>
2021-06-08 16:04:26 +0700

    app func SayHi

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/khancuts/app-func-SayHi
+
+go 1.16
+
+require github.com/khancuts/mod_goLang/v2 v2.0.0-20210608085952-0dfab41ce091
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	modfunchi "github.com/khancuts/mod_goLang/v2"
+)
+
+func main() {
+
+	fmt.Println(modfunchi.SayHi("Abdi"))
+}
